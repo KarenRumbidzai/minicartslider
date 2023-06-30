701163bf91c88060d0c8578ad65e51e7d3d210ac
Karen <kr@vectra.co.za>
2023-06-30 12:28:42 +0100

    VCOM-395, FE renderer update

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,11 +1,7 @@
 var config = {
-    deps: [
-        "Vectra_MiniCartSlider/js/minicartSlider"
-    ],
     map: {
         '*': {
             'Magento_Checkout/template/minicart/content.html': 'Vectra_MiniCartSlider/template/minicart/content.html'
-
-        },
+        }
     }
 }
